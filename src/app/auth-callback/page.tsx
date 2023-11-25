5303eef1b17603df3f6d8e057c826605625e0457
Joshua Natanael <josh.natanael9@gmail.com>
2023-11-25 09:57:53 +0700

    setup auth & trpc

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,14 @@
+import { useRouter, useSearchParams } from 'next/navigation';
+import React from 'react';
+import { trpc } from '../_trpc/client';
+
+const Page = async () => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const origin = searchParams.get('origin');
+
+  const { data, isLoading } = trpc.test.useQuery();
+};
+
+export default Page;
